04950cb822393f23b314f60e4811939d58bd44f7
Lauren Mastroni <laurenmastroni@gmail.com>
2017-11-07 13:50:20 +0800

    Finish sketching in homepage

---
+++ store-locator/panel.js
@@ -0,0 +1,20 @@
+google.maps.event.addDomListener(window, 'load', function() {
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: new google.maps.LatLng(37.7749, -122.4194),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var panelDiv = document.getElementById('panel');
+
+  var data = new MedicareDataSource;
+
+  var view = new storeLocator.View(map, data, {
+    geolocation: false,
+    features: data.getFeatures()
+  });
+
+  new storeLocator.Panel(panelDiv, {
+    view: view
+  });
+});
